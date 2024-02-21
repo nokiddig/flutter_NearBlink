9b82748a6ce2b14be203d73f585e2ea53b3e1751
SyLV <sy2000dn0@gmail.com>
2024-02-21 16:23:31 +0700

    ui: add assets file

---
+++ lib/ui/components/no_account_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:blog_app/utils/constant/my_const.dart';
+
+import '../screen/sign_up/sign_up_screen.dart';
+
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Don’t have an account? ",
+          style: TextStyle(fontSize: 16),
+        ),
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, SignUpScreen.routeName),
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 16, color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/screen/init_screen.dart
@@ -0,0 +1,120 @@
+import 'package:blog_app/ui/screen/profile/profile_screen.dart';
+import 'package:blog_app/ui/tab/category_tab.dart';
+import 'package:blog_app/ui/tab/home_tab.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:blog_app/utils/constant/my_const.dart';
+
+const Color inActiveIconColor = Color(0xFFB6B6B6);
+
+class InitScreen extends StatefulWidget {
+  const InitScreen({super.key});
+
+  static String routeName = "/";
+
+  @override
+  State<InitScreen> createState() => _InitScreenState();
+}
+
+class _InitScreenState extends State<InitScreen> {
+  int currentSelectedIndex = 0;
+
+  void updateCurrentIndex(int index) {
+    setState(() {
+      currentSelectedIndex = index;
+    });
+  }
+
+  final pages = [
+    const HomeTab(),
+    CategoryTab(),
+    const Center(
+      child: Text("Chat"),
+    ),
+    const ProfileScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentSelectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: updateCurrentIndex,
+        currentIndex: currentSelectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/Shop Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                inActiveIconColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/icons/Shop Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                kPrimaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/Heart Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                inActiveIconColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/icons/Heart Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                kPrimaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: "Fav",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/Chat bubble Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                inActiveIconColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/icons/Chat bubble Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                kPrimaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: "Chat",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/User Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                inActiveIconColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/icons/User Icon.svg",
+              colorFilter: const ColorFilter.mode(
+                kPrimaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: "Fav",
+          ),
+        ],
+      ),
+    );
+  }
+}
